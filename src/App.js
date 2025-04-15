082964b6d484d3b7e3213166e47e091224238c61
GabbyTech <sanctagab@gmail.com>
2025-04-15 17:47:02 +0100

    Fresh React JS setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn with us at Edenites, <code>src/App.js</code>, acquire and grow in knowledge.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Edenites Academy
         </a>
       </header>
     </div>
